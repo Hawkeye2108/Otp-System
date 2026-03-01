b4f8d8c5adfdaa43039325582698545b26e860aa
Jatin Rawat <jatinrawat2108@gmail.com>
2026-03-02 00:07:00 +0530

    Modified pom.xml

--- src/main/java/com/example/OtpSystem/service/EmailService.java
+++ src/main/java/com/example/OtpSystem/service/EmailService.java
@@ -5,6 +5,10 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
+import sibApi.TransactionalEmailsApi;
+import sibModel.SendSmtpEmail;
+import sibApi.ApiClient;
+import sibApi.Configuration;
 
 @Service
 public class EmailService {
@@ -15,13 +19,32 @@ public class EmailService {
     @Value("${spring.mail.from}")
     private String fromEmail;
 
-    public void sendMail(String to, String subject, String body){
-        SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setFrom(fromEmail);
-        mail.setTo(to);
-        mail.setSubject(subject);
-        mail.setText(body);
+    @Value("${brevo.api.key}")
+    private String apiKey;
 
-        mailSender.send(mail);
+    public void sendMail(String to, String subject, String body){
+//        SimpleMailMessage mail = new SimpleMailMessage();
+//        mail.setFrom(fromEmail);
+//        mail.setTo(to);
+//        mail.setSubject(subject);
+//        mail.setText(body);
+//
+//        mailSender.send(mail);
+
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setApiKey(apiKey);
+
+        TransactionalEmailsApi apiInstance = new TransactionalEmailsApi();
+
+        SendSmtpEmail email = new SendSmtpEmail();
+
+        email.setSubject(subject);
+        email.setHtmlContent("<html><body>" + body + "</body></html>");
+        email.setTo(List.of(new SendSmtpEmailTo().email(to)));
+        email.setSender(new SendSmtpEmailSender()
+                .email("regularguy2108@gmail.com")
+                .name("Otp System"));
+
+        apiInstance.sendTransacEmail(email);
     }
 }
