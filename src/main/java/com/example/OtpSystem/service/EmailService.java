85c56a262de125994ce0ad4f00a26b3648cc7657
Jatin Rawat <jatinrawat2108@gmail.com>
2026-03-01 21:58:10 +0530

    Modified EmailService.java

--- src/main/java/com/example/OtpSystem/service/EmailService.java
+++ src/main/java/com/example/OtpSystem/service/EmailService.java
@@ -10,8 +10,13 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    // Add sender email from properties
+    @Value("${spring.mail.from}")
+    private String fromEmail;
+
     public void sendMail(String to, String subject, String body){
         SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom(fromEmail);
         mail.setTo(to);
         mail.setSubject(subject);
         mail.setText(body);
